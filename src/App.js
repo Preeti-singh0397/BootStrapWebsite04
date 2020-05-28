1c973289a06eba87c95a1ff697ead157288ee550
Preeti-singh0397 <63150571+Preeti-singh0397@users.noreply.github.com>
2020-05-28 12:34:20 +0530

    Final project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./component/layout/Navbar";
+import Alert from "./component/layout/Alert";
+import About from "./component/pages/About";
+import Home from "./component/pages/Home";
+import NotFound from "./component/pages/NotFound";
 
-function App() {
+import User from "./component/users/User";
+
+import GithubState from "./context/github/githubState";
+import AlertState from "./context/alert/alertState";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div>
+            <Navbar />
+            <Alert />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" component={User} />} />
+              <Route component={NotFound} />} />
+            </Switch>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
